16c5e635939e7b0dc1f1da6b754559e2092cba48
HwangSooHyeon <48857651+HwangSooHyeon@users.noreply.github.com>
2023-02-03 16:54:44 +0900

    feat(home_screen): 상태바 색상 dark로 변경

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.dark);
+
     return Scaffold(
       body: PageView(
         children: [1, 2, 3, 4, 5, 6]
